0d3b9a960b866e32fc404c1f1190cecc9f4fd12c
Matej404 <matejjurin278@gmail.com>
2022-07-25 22:15:02 +0100

    home

---
+++ src/web/Main.js
@@ -0,0 +1,11 @@
+import "./style.css";
+import Home from "./home/Home";
+
+export default function Main() {
+
+    return(
+        <div>
+            <Home />
+        </div>
+    )
+}
---
+++ src/web/home/Home.js
@@ -0,0 +1,12 @@
+import "./home.css";
+
+export default function Home() {
+
+    return(
+        <div className="home">
+            <h1>Welcome</h1>
+            <h1>to</h1>
+            <h1>Primošten</h1>
+        </div>
+    )
+}
