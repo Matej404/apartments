fd1f89ba88d2a77c8b59f6a79b5798fdc6f7925d
Matej404 <matejjurin278@gmail.com>
2022-07-26 09:01:48 +0100

    header

--- src/web/home/Home.js
+++ src/web/home/Home.js
@@ -1,9 +1,11 @@
 import "./home.css";
+import Header from "./header/Header";
 
 export default function Home() {
 
     return(
         <div className="home">
+            <Header />
             <h1>Welcome</h1>
             <h1>to</h1>
             <h1>Primošten</h1>
---
+++ src/web/home/header/Header.js
@@ -0,0 +1,15 @@
+import "./header.css";
+
+export default function Header() {
+
+    return(
+        <div className="header">
+            <ol className="header-list">
+                <li>HOME</li>
+                <li>ABOUT US</li>
+                <li>OFFERS</li>
+                <li>CONTACT</li>
+            </ol>
+        </div>
+    )
+}
