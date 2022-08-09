203270bfbf2461bbf88658c5af2ce229682143e9
Matej404 <matejjurin278@gmail.com>
2022-08-09 10:16:16 +0100

    offers-different way

---
+++ src/web/location/offers/OffersData.js
@@ -0,0 +1,134 @@
+//apartman1
+import AP1Room1 from "./photos/apartman1/1.1.jpg";
+import AP1Room2 from "./photos/apartman1/1.2.jpg";
+import AP1Room3 from "./photos/apartman1/1.3.jpg";
+import AP1Room4 from "./photos/apartman1/1.4.jpg";
+import AP1Room5 from "./photos/apartman1/1.5.jpg";
+import AP1Room6 from "./photos/apartman1/1.6.jpg";
+import AP1Room7 from "./photos/apartman1/1.7.jpg";
+import AP1Room8 from "./photos/apartman1/1.8.jpg";
+import AP1Room9 from "./photos/apartman1/1.9.jpg";
+import AP1Room10 from "./photos/apartman1/1.10.jpg";
+import AP1Room11 from "./photos/apartman1/1.11.jpg";
+import AP1Room12 from "./photos/apartman1/1.12.jpg";
+
+//apartman2
+import AP2Room1 from "./photos/apartman2/2.1.JPG";
+import AP2Room2 from "./photos/apartman2/2.2.JPG";
+import AP2Room3 from "./photos/apartman2/2.3.JPG";
+import AP2Room4 from "./photos/apartman2/2.4.JPG";
+import AP2Room5 from "./photos/apartman2/2.5.JPG";
+import AP2Room6 from "./photos/apartman2/2.6.JPG";
+import AP2Room7 from "./photos/apartman2/2.7.JPG";
+import AP2Room8 from "./photos/apartman2/2.8.JPG";
+import AP2Room9 from "./photos/apartman2/2.9.JPG";
+
+//apartman3
+import AP3Room1 from "./photos/apartman3/3.1.JPG";
+import AP3Room2 from "./photos/apartman3/3.2.JPG";
+import AP3Room3 from "./photos/apartman3/3.3.JPG";
+import AP3Room4 from "./photos/apartman3/3.4.JPG";
+import AP3Room5 from "./photos/apartman3/3.5.JPG";
+
+
+export const OffersData = [
+    {
+        id: 1,
+        name: "1.",
+        text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley",
+        rooms: [
+            {
+                room: AP1Room1,
+            },
+            {
+                room: AP1Room2,
+            },
+            {
+                room: AP1Room3,
+            },
+            {
+                room: AP1Room4,
+            },
+            {
+                room: AP1Room5,
+            },
+            {
+                room: AP1Room6,
+            },
+            {
+                room: AP1Room7,
+            },
+            {
+                room: AP1Room8,
+            },
+            {
+                room: AP1Room9,
+            },
+            {
+                room: AP1Room10,
+            },
+            {
+                room: AP1Room11,
+            },
+            {
+                room: AP1Room12,
+            },
+        ]
+    },
+    {
+        id: 2,
+        name: "2.",
+        text: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution",
+        rooms: [
+            {
+                room: AP2Room1,
+            },
+            {
+                room: AP2Room2,
+            },
+            {
+                room: AP2Room3,
+            },
+            {
+                room: AP2Room4,
+            },
+            {
+                room: AP2Room5,
+            },
+            {
+                room: AP2Room6,
+            },
+            {
+                room: AP2Room7,
+            },
+            {
+                room: AP2Room8,
+            },
+            {
+                room: AP2Room9,
+            },
+        ]
+    },
+    {
+        id: 3,
+        name: "3.",
+        text: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin",
+        rooms: [
+            {
+                room: AP3Room1,
+            },
+            {
+                room: AP3Room2,
+            },
+            {
+                room: AP3Room3,
+            },
+            {
+                room: AP3Room4,
+            },
+            {
+                room: AP3Room5,
+            }
+        ]
+    }
+]
